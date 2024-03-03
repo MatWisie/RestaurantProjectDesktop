a03bbb548e1f40d201b7fd226f1d6e93b85d24ae
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-03 21:22:39 +0100

    Added MainWindowViewModel

--- RestaurantDesktop/MainWindow.xaml.cs
+++ RestaurantDesktop/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestaurantDesktop.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RestaurantDesktop
 {
@@ -19,6 +12,7 @@ namespace RestaurantDesktop
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = App.Current.Services.GetService<MainWindowViewModel>();
         }
     }
 }
