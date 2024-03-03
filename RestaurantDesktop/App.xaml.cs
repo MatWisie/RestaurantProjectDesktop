5597d7326bd2b1a22275f3d3556a2576a2098c0f
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-03 21:22:19 +0100

    Added DI to App.xaml.cs

--- RestaurantDesktop/App.xaml.cs
+++ RestaurantDesktop/App.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestaurantDesktop.ViewModel;
 using System.Windows;
 
 namespace RestaurantDesktop
@@ -9,6 +9,21 @@ namespace RestaurantDesktop
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+        public new static App Current => (App)Application.Current;
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<LoginViewModel>();
+            services.AddTransient<MainWindowViewModel>();
+
+            return services.BuildServiceProvider();
+        }
     }
 
 }
