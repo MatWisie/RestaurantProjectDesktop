f835752e60a8b19cc3142111596fa0928ee6f2de
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-06 19:48:49 +0100

    Added ValidateLogin_CorrectData_EmptyString test

--- RestaurantDesktopTests/LoginServiceTest.cs
+++ RestaurantDesktopTests/LoginServiceTest.cs
@@ -6,5 +6,15 @@ namespace RestaurantDesktopTests
     [TestClass]
     public class LoginServiceTest
     {
+        [TestMethod]
+        public void ValidateLogin_CorrectData_EmptyString()
+        {
+            var authRepository = new AuthRepository();
+            AuthService authService = new AuthService(authRepository);
+
+            var result = authService.ValidateLogin("Mateusz", "Ppppp1!");
+            Assert.AreEqual(string.Empty, result);
+        }
+
     }
 }
