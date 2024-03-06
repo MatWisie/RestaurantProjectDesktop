d87f3e18def1fd6cbf6ae7ec2dcf18be2c8d14d0
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-06 19:30:23 +0100

    Added AuthRepository and IAuthRepository with LoginWorker method

---
+++ RestaurantDesktop/Interface/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿using RestSharp;
+
+namespace RestaurantDesktop.Interface
+{
+    public interface IAuthRepository
+    {
+        RestResponse LoginWorker(string username, string password);
+    }
+}
