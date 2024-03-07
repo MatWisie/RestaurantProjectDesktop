a1e8eff98728dab74a6f0bc4d513bb016d750362
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-07 20:50:29 +0100

    Extracted token and userid from json and saved it in configuration

--- RestaurantDesktop/ViewModel/LoginViewModel.cs
+++ RestaurantDesktop/ViewModel/LoginViewModel.cs
@@ -1,17 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RestaurantDesktop.Interface;
-using System.Windows;
 
 namespace RestaurantDesktop.ViewModel
 {
     public partial class LoginViewModel : ObservableObject
     {
         private readonly IAuthService _authService;
+        private readonly IConfigurationService _configurationService;
+        private readonly IJsonService _jsonService;
 
-        public LoginViewModel(IAuthService authService)
+        public LoginViewModel(IAuthService authService, IConfigurationService configurationService, IJsonService jsonService)
         {
             _authService = authService;
+            _configurationService = configurationService;
+            _jsonService = jsonService;
         }
 
         [RelayCommand]
@@ -29,8 +32,14 @@ namespace RestaurantDesktop.ViewModel
                 ErrorText = "Wrong name or password";
                 return;
             }
-            MessageBox.Show(response.Content);
 
+            if (response.Content != null)
+            {
+                string token = _jsonService.ExtractFromJson(response.Content, "token");
+                string userId = _jsonService.ExtractFromJson(response.Content, "userId");
+                _configurationService.AddConfiguration("UserToken", token);
+                _configurationService.AddConfiguration("UserId", userId);
+            }
         }
 
         [ObservableProperty]
