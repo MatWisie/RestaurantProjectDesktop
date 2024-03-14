365943bb99295752d380147db9ec75f6b56caa9d
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-14 20:01:32 +0100

    Added UserService

---
+++ RestaurantDesktop/Service/UserService.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantDesktop.Interface;
+using RestSharp;
+
+namespace RestaurantDesktop.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUsersRepository _usersRepository;
+        private readonly IConfigurationService _configurationService;
+        public UserService(IUsersRepository usersRepository, IConfigurationService configurationService)
+        {
+            _usersRepository = usersRepository;
+            _configurationService = configurationService;
+        }
+        public RestResponse GetUsers(string userToken)
+        {
+            return _usersRepository.GetUsers(userToken);
+        }
+    }
+}
