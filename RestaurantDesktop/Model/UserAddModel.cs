0d419fa195288203c1c9dcb6bfb7fbc13e9d597e
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-17 12:58:19 +0100

    Added UserAddModel

---
+++ RestaurantDesktop/Model/UserAddModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantDesktop.Model
+{
+    public class UserAddModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+    }
+}
