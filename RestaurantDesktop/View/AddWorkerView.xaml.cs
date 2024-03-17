5501183fb9db2143753c71bc704e08ddf98866fe
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-17 13:01:13 +0100

    Added AddWorkerView

---
+++ RestaurantDesktop/View/AddWorkerView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using RestaurantDesktop.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RestaurantDesktop.View
+{
+    /// <summary>
+    /// Interaction logic for AddWorkerView.xaml
+    /// </summary>
+    public partial class AddWorkerView : UserControl
+    {
+        public AddWorkerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
