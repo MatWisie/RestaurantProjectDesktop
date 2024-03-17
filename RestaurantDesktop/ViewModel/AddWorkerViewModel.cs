acf051f7077b6c10321c9b76dc378a0550893844
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-17 13:01:59 +0100

    Added AddWorkerViewModel

---
+++ RestaurantDesktop/ViewModel/AddWorkerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantDesktop.Interface;
+using RestaurantDesktop.Model;
+using RestaurantDesktop.Model.Message;
+using System.Text.RegularExpressions;
+
+namespace RestaurantDesktop.ViewModel
+{
+    public partial class AddWorkerViewModel : ObservableObject
+    {
+        }
+    }
+}
