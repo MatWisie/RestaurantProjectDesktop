d84e41d4f617018873dcf8ec1a0c618fe06d94dc
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-17 18:44:35 +0100

    Added PasswordPatternStatic with PasswordPattern

---
+++ RestaurantDesktop/Model/PasswordPatternStatic.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RestaurantDesktop.Model
+{
+    public static class PasswordPatternStatic
+    {
+        public static Regex PasswordPattern => new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$");
+    }
+}
