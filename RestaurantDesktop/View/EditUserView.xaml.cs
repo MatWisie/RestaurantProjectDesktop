6d06a5154e0a7af80bed4c133b8dc14a92ef4e29
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-17 18:45:33 +0100

    Added EditUserView

---
+++ RestaurantDesktop/View/EditUserView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using RestaurantDesktop.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RestaurantDesktop.View
+{
+    /// <summary>
+    /// Interaction logic for EditUserView.xaml
+    /// </summary>
+    public partial class EditUserView : UserControl
+    {
+        public EditUserView()
+        {
+            InitializeComponent();
+        }
+
+        private void UserPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is EditUserViewModel editUserViewModel)
+            {
+                editUserViewModel.Password = UserPassword.Password;
+            }
+        }
+
+        private void ConfirmUserPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is EditUserViewModel editUserViewModel)
+            {
+                editUserViewModel.ConfirmPassword = ConfirmUserPassword.Password;
+            }
+        }
+    }
+}
