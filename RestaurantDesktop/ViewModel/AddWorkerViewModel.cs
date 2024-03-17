73234713fb29c2764a3495369662f3ed8dff8a75
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-17 13:02:28 +0100

    Added needed register properties to AddWorkerViewModel

--- RestaurantDesktop/ViewModel/AddWorkerViewModel.cs
+++ RestaurantDesktop/ViewModel/AddWorkerViewModel.cs
@@ -11,6 +11,33 @@ namespace RestaurantDesktop.ViewModel
 {
     public partial class AddWorkerViewModel : ObservableObject
     {
+        private readonly IUserService _userService;
+        private readonly IConfigurationService _configurationService;
+        private readonly IAuthService _authService;
+        public AddWorkerViewModel(IUserService userService, IConfigurationService configurationService, IAuthService authService)
+        {
+            _userService = userService;
+            _configurationService = configurationService;
+            _authService = authService;
+        }
+
+        [ObservableProperty]
+        private string userName;
+
+        [ObservableProperty]
+        private string email;
+
+        [ObservableProperty]
+        private string password;
+
+        [ObservableProperty]
+        private string confirmPassword;
+
+        [ObservableProperty]
+        private int age;
+
+        [ObservableProperty]
+        private string errorText;
         }
     }
 }
