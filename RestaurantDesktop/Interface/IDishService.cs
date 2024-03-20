3dfc996ae6fac44973c4a723c87dc748b5dc2e49
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-20 22:07:57 +0100

    Added DishService and IDishService

---
+++ RestaurantDesktop/Interface/IDishService.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantDesktop.Model;
+using RestSharp;
+
+namespace RestaurantDesktop.Interface
+{
+    public interface IDishService
+    {
+        RestResponse AddDish(string userToken, DishModel dishModel);
+        Task<RestResponse> DeleteDish(string userToken, string dishIdToDelete);
+        RestResponse EditDish(string userToken, DishModel dishEditModel, string dishIdToEdit);
+        Task<RestResponse> GetDishes(string userToken);
+    }
+}
---
+++ RestaurantDesktop/Service/DishService.cs
@@ -0,0 +1,37 @@
+﻿using RestaurantDesktop.Interface;
+using RestaurantDesktop.Model;
+using RestSharp;
+using System.Text.Json;
+
+namespace RestaurantDesktop.Service
+{
+    public class DishService : IDishService
+    {
+        private readonly IDishesRepository _dishesRepository;
+        public DishService(IDishesRepository dishesRepository)
+        {
+            _dishesRepository = dishesRepository;
+        }
+        public async Task<RestResponse> GetDishes(string userToken)
+        {
+            return await _dishesRepository.GetDishes(userToken);
+        }
+
+        public RestResponse AddDish(string userToken, DishModel dishModel)
+        {
+            string json = JsonSerializer.Serialize(dishModel);
+            return _dishesRepository.AddDish(userToken, json);
+        }
+
+        public RestResponse EditDish(string userToken, DishModel dishEditModel, string dishIdToEdit)
+        {
+            string json = JsonSerializer.Serialize(dishEditModel);
+            return _dishesRepository.EditDish(userToken, json, dishIdToEdit);
+        }
+
+        public async Task<RestResponse> DeleteDish(string userToken, string dishIdToDelete)
+        {
+            return await _dishesRepository.DeleteDish(userToken, dishIdToDelete);
+        }
+    }
+}
