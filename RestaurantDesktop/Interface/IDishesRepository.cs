db83d9ea6d7a4e63a1c3202a9ff160132d1234b9
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-20 22:07:44 +0100

    Added DishRepository and IDishRepository

---
+++ RestaurantDesktop/Interface/IDishesRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestSharp;
+
+namespace RestaurantDesktop.Interface
+{
+    public interface IDishesRepository
+    {
+        RestResponse AddDish(string userToken, string json);
+        Task<RestResponse> DeleteDish(string userToken, string dishIdToDelete);
+        RestResponse EditDish(string userToken, string json, string dishIdToEdit);
+        Task<RestResponse> GetDishes(string userToken);
+    }
+}
---
+++ RestaurantDesktop/Repository/DishesRepository.cs
@@ -0,0 +1,55 @@
+﻿using RestaurantDesktop.Interface;
+using RestSharp;
+
+namespace RestaurantDesktop.Repository
+{
+    public class DishesRepository : IDishesRepository
+    {
+        public async Task<RestResponse> GetDishes(string userToken)
+        {
+            var options = new RestClientOptions(Connection.ApiAddress);
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/Dish", Method.Get);
+
+            request.AddHeader("Authorization", $"Bearer {userToken}");
+
+            return await client.ExecuteAsync(request);
+        }
+
+        public RestResponse AddDish(string userToken, string json)
+        {
+            var options = new RestClientOptions(Connection.ApiAddress);
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/Dish", Method.Post);
+            request.AddJsonBody(json);
+
+            request.AddHeader("Authorization", $"Bearer {userToken}");
+
+            return client.Execute(request);
+        }
+
+        public RestResponse EditDish(string userToken, string json, string dishIdToEdit)
+        {
+            var options = new RestClientOptions(Connection.ApiAddress);
+            var client = new RestClient(options);
+            var request = new RestRequest($"/api/Dish/{dishIdToEdit}", Method.Put);
+            request.AddJsonBody(json);
+
+            request.AddHeader("Authorization", $"Bearer {userToken}");
+
+            return client.Execute(request);
+        }
+
+        public async Task<RestResponse> DeleteDish(string userToken, string dishIdToDelete)
+        {
+            var options = new RestClientOptions(Connection.ApiAddress);
+            var client = new RestClient(options);
+
+            var request = new RestRequest($"/api/Dish/{dishIdToDelete}", Method.Delete);
+
+            request.AddHeader("Authorization", $"Bearer {userToken}");
+
+            return await client.ExecuteAsync(request);
+        }
+    }
+}
