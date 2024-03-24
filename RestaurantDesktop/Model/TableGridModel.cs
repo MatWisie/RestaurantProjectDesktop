084d292ac79981810d6c403c6d5927997aaede8b
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-24 13:15:25 +0100

    Added TableGridModel

---
+++ RestaurantDesktop/Model/TableGridModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantDesktop.Model
+{
+    public class TableGridModel
+    {
+        public int NumberOfRows { get; set; }
+        public int NumberOfColumns { get; set; }
+    }
+}
