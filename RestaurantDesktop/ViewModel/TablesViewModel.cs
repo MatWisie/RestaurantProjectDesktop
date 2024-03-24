cc64e89078f820004a60dd48a8e0345435bb0116
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-24 13:16:52 +0100

    Added BuildTableGrid method to TablesViewModel

--- RestaurantDesktop/ViewModel/TablesViewModel.cs
+++ RestaurantDesktop/ViewModel/TablesViewModel.cs
@@ -23,7 +23,39 @@ namespace RestaurantDesktop.ViewModel
             _authService = authService;
             _jsonService = jsonService;
 
+            BuildTableGrid();
 
+        }
+
+        [ObservableProperty]
+        private Grid tableGrid;
+
+        private async void BuildTableGrid()
+        {
+            MessageService.SendLoadingBegin();
+            var dishResponse = await _gridService.GetGrid(_configurationService.GetConfiguration("UserToken"));
+            var tablesResponse = await _tableService.GetTables(_configurationService.GetConfiguration("UserToken"));
+            _authService.CheckIfLogout(dishResponse.StatusCode);
+            _authService.CheckIfLogout(tablesResponse.StatusCode);
+            if (dishResponse.IsSuccessful && dishResponse.Content != null && tablesResponse.IsSuccessful && tablesResponse.Content != null)
+            {
+                TableGridModel gridData = _jsonService.ExtractTableGridDataFromJson(dishResponse.Content);
+                TableGrid = _gridService.BuildGrid(gridData);
+
+                List<TableWithIdModel> tables = _jsonService.ExtractTablesFromJson(tablesResponse.Content);
+                foreach (var table in tables)
+                {
+                    PackIconBoxIcons tableIcon = new PackIconBoxIcons()
+                    {
+                        Kind = PackIconBoxIconsKind.RegularChair,
+                        DataContext = table
+                    };
+                    Grid.SetRow(tableIcon, table.GridRow);
+                    Grid.SetColumn(tableIcon, table.GridColumn);
+                    TableGrid.Children.Add(tableIcon);
+                }
+            }
+            MessageService.SendLoadingEnd();
         }
     }
 }
