efd9c8d9c4a484525c449aef65a532d700365c96
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-24 13:12:51 +0100

    Added GridService and IGridService

---
+++ RestaurantDesktop/Interface/IGridService.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantDesktop.Model;
+using RestSharp;
+using System.Windows.Controls;
+
+namespace RestaurantDesktop.Interface
+{
+    public interface IGridService
+    {
+        RestResponse EditGrid(string userToken, TableGridModel tableEditModel);
+        Task<RestResponse> GetGrid(string userToken);
+        Grid BuildGrid(TableGridModel gridData);
+    }
+}
---
+++ RestaurantDesktop/Service/GridService.cs
@@ -0,0 +1,43 @@
+﻿using RestaurantDesktop.Interface;
+using RestaurantDesktop.Model;
+using RestSharp;
+using System.Text.Json;
+using System.Windows.Controls;
+
+namespace RestaurantDesktop.Service
+{
+    public class GridService : IGridService
+    {
+        private readonly IGridRepository _gridRepository;
+        public GridService(IGridRepository gridRepository)
+        {
+            _gridRepository = gridRepository;
+        }
+        public async Task<RestResponse> GetGrid(string userToken)
+        {
+            return await _gridRepository.GetGrid(userToken);
+        }
+        public RestResponse EditGrid(string userToken, TableGridModel tableEditModel)
+        {
+            string json = JsonSerializer.Serialize(tableEditModel);
+            return _gridRepository.EditGrid(userToken, json);
+        }
+
+        public Grid BuildGrid(TableGridModel gridData)
+        {
+            Grid tmpGrid = new Grid();
+            for (int i = 0; i < gridData.NumberOfRows; i++)
+            {
+                tmpGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int i = 0; i < gridData.NumberOfColumns; i++)
+            {
+                tmpGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            tmpGrid.ShowGridLines = true;
+            return tmpGrid;
+        }
+    }
+}
