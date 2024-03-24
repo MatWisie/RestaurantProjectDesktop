b24211f579fd9c079d6ad52289b2f777bf7ae6dc
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-24 13:15:31 +0100

    Added TableModel

---
+++ RestaurantDesktop/Model/TableModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantDesktop.Model
+{
+    public class TableModel
+    {
+        public bool IsAvailable { get; set; } = true;
+        public int NumberOfSeats { get; set; }
+        public int GridRow { get; set; }
+        public int GridColumn { get; set; }
+    }
+}
