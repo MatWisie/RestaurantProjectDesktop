4191448655f01ebb3cf45744ab87011655fb28de
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-24 13:16:37 +0100

    Added TablesViewModel

---
+++ RestaurantDesktop/ViewModel/TablesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MahApps.Metro.IconPacks;
+using RestaurantDesktop.Interface;
+using RestaurantDesktop.Model;
+using RestaurantDesktop.Service;
+using System.Windows.Controls;
+
+namespace RestaurantDesktop.ViewModel
+{
+    public partial class TablesViewModel : ObservableObject
+    {
+        private readonly IGridService _gridService;
+        private readonly ITableService _tableService;
+        private readonly IConfigurationService _configurationService;
+        private readonly IAuthService _authService;
+        private readonly IJsonService _jsonService;
+
+        public TablesViewModel(IGridService gridService, ITableService tableService, IConfigurationService configurationService, IAuthService authService, IJsonService jsonService)
+        {
+            _gridService = gridService;
+            _tableService = tableService;
+            _configurationService = configurationService;
+            _authService = authService;
+            _jsonService = jsonService;
+
+
+        }
+    }
+}
