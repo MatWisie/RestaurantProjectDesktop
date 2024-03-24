98075a5f28a11b1da1cf269d9995ecfe18a89cd7
MatWisie <Mateuszwisiecki@gmail.com>
2024-03-24 13:13:09 +0100

    Added TablesRepository and ITablesRepository

---
+++ RestaurantDesktop/Interface/ITablesRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestSharp;
+
+namespace RestaurantDesktop.Interface
+{
+    public interface ITablesRepository
+    {
+        RestResponse AddTable(string userToken, string json);
+        Task<RestResponse> DeleteTable(string userToken, string tableIdToDelete);
+        RestResponse EditTable(string userToken, string json, string tableIdToEdit);
+        Task<RestResponse> GetTables(string userToken);
+    }
+}
