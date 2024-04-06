fc4fe11a06a3f78f106b17f50b7eae979b4420e3
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-06 13:27:27 +0200

    Added Order model

---
+++ RestaurantDesktop/Model/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantDesktop.Model
+{
+    public class Order
+    {
+        public StatusEnum Status { get; set; }
+        public double Price { get; set; }
+        public List<DishWithIdModel> Dishes { get; set; }
+        public int TableId { get; set; }
+        public string UserId { get; set; }
+    }
+}
