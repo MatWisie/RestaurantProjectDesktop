a383348f0f7672f28aa7feeef302f6adf74e00e7
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-06 13:28:11 +0200

    Added OrdersService with GetOrders method

---
+++ RestaurantDesktop/Interface/IOrdersService.cs
@@ -0,0 +1,9 @@
+﻿using RestSharp;
+
+namespace RestaurantDesktop.Interface
+{
+    public interface IOrdersService
+    {
+        Task<RestResponse> GetOrders(string userToken);
+    }
+}
---
+++ RestaurantDesktop/Service/OrdersService.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantDesktop.Interface;
+using RestSharp;
+
+namespace RestaurantDesktop.Service
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly IOrdersRepository _ordersRepository;
+
+        public OrdersService(IOrdersRepository ordersRepository)
+        {
+            _ordersRepository = ordersRepository;
+        }
+
+        public async Task<RestResponse> GetOrders(string userToken)
+        {
+            return await _ordersRepository.GetOrders(userToken);
+        }
+    }
+}
