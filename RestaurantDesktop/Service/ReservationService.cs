501d60b8e02f097b47168edbb91c44ac96ea4387
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-15 20:59:52 +0200

    Added GetReservations to ReservationService

--- RestaurantDesktop/Service/ReservationService.cs
+++ RestaurantDesktop/Service/ReservationService.cs
@@ -7,5 +7,16 @@ namespace RestaurantDesktop.Service
 {
     public class ReservationService : IReservationService
     {
+        private readonly IReservationsRepository _reservationsRepository;
+        public ReservationService(IReservationsRepository reservationsRepository)
+        {
+            _reservationsRepository = reservationsRepository;
+        }
+
+        public async Task<RestResponse> GetReservations(string userToken)
+        {
+            return await _reservationsRepository.GetReservations(userToken);
+        }
+
     }
 }
