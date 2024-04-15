6aa24a2d8dd0e2a9088992aea78c9147a803d073
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-15 20:58:35 +0200

    Added GetReservations to ReservationsRepository

--- RestaurantDesktop/Interface/IReservationsRepository.cs
+++ RestaurantDesktop/Interface/IReservationsRepository.cs
@@ -3,5 +3,9 @@
 namespace RestaurantDesktop.Interface
 {
     public interface IReservationsRepository
-    {    }
+    {
+        Task<RestResponse> DeleteReservation(string userToken, string reservationId);
+        Task<RestResponse> EditReservation(string userToken, string json, string reservationId);
+        Task<RestResponse> GetReservations(string userToken);
+    }
 }
--- RestaurantDesktop/Repository/ReservationsRepository.cs
+++ RestaurantDesktop/Repository/ReservationsRepository.cs
@@ -5,5 +5,16 @@ namespace RestaurantDesktop.Repository
 {
     public class ReservationsRepository : IReservationsRepository
     {
+        public async Task<RestResponse> GetReservations(string userToken)
+        {
+            var options = new RestClientOptions(Connection.ApiAddress);
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/Reservation", Method.Get);
+
+            request.AddHeader("Authorization", $"Bearer {userToken}");
+
+            return await client.ExecuteAsync(request);
+        }
+
     }
 }
