f419bb4873cc56a5c8cb768cdc28292841b19540
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-15 20:59:07 +0200

    Added DeleteReservation to ReservationsRepository

--- RestaurantDesktop/Repository/ReservationsRepository.cs
+++ RestaurantDesktop/Repository/ReservationsRepository.cs
@@ -28,5 +28,16 @@ namespace RestaurantDesktop.Repository
             return client.ExecuteAsync(request);
         }
 
+        public async Task<RestResponse> DeleteReservation(string userToken, string reservationId)
+        {
+            var options = new RestClientOptions(Connection.ApiAddress);
+            var client = new RestClient(options);
+
+            var request = new RestRequest($"/ReservationDelete-Worker/{reservationId}", Method.Delete);
+
+            request.AddHeader("Authorization", $"Bearer {userToken}");
+
+            return await client.ExecuteAsync(request);
+        }
     }
 }
