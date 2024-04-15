e47d2859c3c0a84a24b67a8189ea788a86729686
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-15 20:58:49 +0200

    Added EditReservation to ReservationsRepository

--- RestaurantDesktop/Repository/ReservationsRepository.cs
+++ RestaurantDesktop/Repository/ReservationsRepository.cs
@@ -16,5 +16,17 @@ namespace RestaurantDesktop.Repository
             return await client.ExecuteAsync(request);
         }
 
+        public Task<RestResponse> EditReservation(string userToken, string json, string reservationId)
+        {
+            var options = new RestClientOptions(Connection.ApiAddress);
+            var client = new RestClient(options);
+            var request = new RestRequest($"/api/Reservation/{reservationId}", Method.Put);
+            request.AddJsonBody(json);
+
+            request.AddHeader("Authorization", $"Bearer {userToken}");
+
+            return client.ExecuteAsync(request);
+        }
+
     }
 }
