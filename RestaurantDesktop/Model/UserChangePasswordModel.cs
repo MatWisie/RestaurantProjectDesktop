d2bcf0a32aa530ea33d55b5bf5cf115dfaac7312
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-20 13:49:41 +0200

    Added UserChangePasswordModel

---
+++ RestaurantDesktop/Model/UserChangePasswordModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantDesktop.Model
+{
+    public class UserChangePasswordModel
+    {
+        public string userId { get; set; }
+        public string password { get; set; }
+        public string newPassword { get; set; }
+    }
+}
