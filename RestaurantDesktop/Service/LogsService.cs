7da8cd3fb958fa89218d24f9231751df08c8624a
MatWisie <Mateuszwisiecki@gmail.com>
2024-04-28 21:58:09 +0200

    Added LogsService and ILogsService

---
+++ RestaurantDesktop/Service/LogsService.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantDesktop.Interface;
+using RestSharp;
+
+namespace RestaurantDesktop.Service
+{
+    public class LogsService : ILogsService
+    {
+        private readonly ILogsRepository _logsRepository;
+        public LogsService(ILogsRepository logsRepository)
+        {
+            _logsRepository = logsRepository;
+        }
+
+    }
+}
